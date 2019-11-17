938e9799810bc56d19a2dcaaa13c691e75f34806
Marc-Andre Rohde <marc-andre.rohde@runbox.com>
2019-11-17 14:16:54 +0100

    Struktur erstellt. Pagewechsel in frame funktioniert fuer 2 buttons (Home, TT)

---
+++ Frontend/Frontend/View/HomePage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Frontend.View
+{
+    /// <summary>
+    /// Interaction logic for HomeWindow.xaml
+    /// </summary>
+    public partial class HomePage : Page
+    {
+        public HomePage()
+        {
+            InitializeComponent();
+        }
+
+        private void Timetable_Button_Click(object sender, RoutedEventArgs e)
+        {
+            TimetablePage timetablePage = new TimetablePage();
+            this.NavigationService.Navigate(timetablePage);
+        }
+
+        private void Home_Button_Click(object sender, RoutedEventArgs e)
+        {
+            HomePage homePage = new HomePage();
+            this.NavigationService.Navigate(homePage);
+        }
+    }
+}
---
+++ Frontend/Frontend/View/RootPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Frontend.View
+{
+    /// <summary>
+    /// Interaktionslogik für RootPage.xaml
+    /// </summary>
+    public partial class RootPage : Page
+    {
+        public RootPage()
+        {
+            InitializeComponent();
+        }
+        private void Timetable_Button_Click(object sender, RoutedEventArgs e)
+        {
+            TimetablePage timetablePage = new TimetablePage();
+            MainFrame.NavigationService.Navigate(timetablePage);
+        }
+
+        private void Home_Button_Click(object sender, RoutedEventArgs e)
+        {
+            HomePage homePage = new HomePage();
+            MainFrame.NavigationService.Navigate(homePage);
+        }
+    }
+}
