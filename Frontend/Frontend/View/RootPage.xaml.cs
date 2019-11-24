1a45e3e47a8035f0efb879210cd8afdc359736d5
Marc-Andre Rohde <marc-andre.rohde@runbox.com>
2019-11-24 19:24:44 +0100

    +missing font; +rm unneeded ns; +codecleanup 2.0

--- Frontend/Frontend/View/RootPage.xaml.cs
+++ Frontend/Frontend/View/RootPage.xaml.cs
@@ -1,5 +1,4 @@
-﻿using System.Threading;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
 
 namespace Frontend.View
@@ -25,7 +24,7 @@ namespace Frontend.View
         * Methode fuer Eventhandling "Click auf HomeButton" 
         * </summary>
         */
-        private void Home_Button_Click(object sender, RoutedEventArgs e)
+        private void HomeButton_Click(object sender, RoutedEventArgs e)
         {
             HomePage homePage = new HomePage();
             MainFrame.NavigationService.Navigate(homePage);
@@ -35,7 +34,7 @@ namespace Frontend.View
         * Methode fuer Eventhandling "Click auf TimetableButton" 
         * </summary>
         */
-        private void Timetable_Button_Click(object sender, RoutedEventArgs e)
+        private void TimetableButton_Click(object sender, RoutedEventArgs e)
         {
             TimetablePage timetablePage = new TimetablePage();
             MainFrame.NavigationService.Navigate(timetablePage);
@@ -45,7 +44,7 @@ namespace Frontend.View
         * Methode fuer Eventhandling "Click auf SharingServiceButton" 
         * </summary>
         */
-        private void SharingService_Button_Click(object sender, RoutedEventArgs e)
+        private void SharingServiceButton_Click(object sender, RoutedEventArgs e)
         {
             SharingServicePage sharingServicePage = new SharingServicePage();
             MainFrame.NavigationService.Navigate(sharingServicePage);
@@ -55,7 +54,7 @@ namespace Frontend.View
         * Methode fuer Eventhandling "Click auf PersonalDataButton" 
         * </summary>
         */
-        private void PersonalData_Button_Click(object sender, RoutedEventArgs e)
+        private void PersonalDataButton_Click(object sender, RoutedEventArgs e)
         {
             PersonalDataPage personalDataPage = new PersonalDataPage();
             MainFrame.NavigationService.Navigate(personalDataPage);
@@ -65,7 +64,7 @@ namespace Frontend.View
         * Methode fuer Eventhandling "Click auf AdminButton" 
         * </summary>
         */
-        private void Admin_Button_Click(object sender, RoutedEventArgs e)
+        private void AdminButton_Click(object sender, RoutedEventArgs e)
         {
             AdminPage adminPage = new AdminPage();
             MainFrame.NavigationService.Navigate(adminPage);
@@ -75,7 +74,7 @@ namespace Frontend.View
         * Methode fuer Eventhandling "Click auf LogoutButton" 
         * </summary>
         */
-        private void Logout_Button_Click(object sender, RoutedEventArgs e)
+        private void LogoutButton_Click(object sender, RoutedEventArgs e)
         {
             HomePage homePage = new HomePage();
             MainFrame.NavigationService.Navigate(homePage);
