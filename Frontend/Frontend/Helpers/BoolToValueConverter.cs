79726fe91e8c35f184b583f9daa6f4625b4859ab
Marc-Andre Rohde <marc-andre.rohde@runbox.com>
2019-11-26 13:31:24 +0100

    Created & Added Value Converters

---
+++ Frontend/Frontend/Helpers/BoolToValueConverter.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Markup;
+
+namespace Frontend.Helpers
+{
+    /// <summary>
+    /// Use as the base class for BoolToXXX style converters
+    /// </summary>
+    /// <typeparam name="T"></typeparam>    
+    public abstract class BoolToValueConverter<T> : MarkupExtension, IValueConverter
+    {
+        protected BoolToValueConverter()
+        {
+            this.TrueValue = default;
+            this.FalseValue = default;
+        }
+
+        public T FalseValue { get; set; }
+        public T TrueValue { get; set; }
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return System.Convert.ToBoolean(value) ? this.TrueValue : this.FalseValue;
+        }
+
+        public virtual object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value.Equals(this.TrueValue);
+        }
+
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            return this;
+        }
+    }
+}
