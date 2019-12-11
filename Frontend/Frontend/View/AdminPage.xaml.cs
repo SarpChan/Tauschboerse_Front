1e91496c3b71d22b5fce71f5de5d3a7b0f0d255f
Leonie Hilgert <leonieluise.hilgert@student.hs-rm.de>
2019-12-11 15:07:16 +0100

    added ComboBox Style

--- Frontend/Frontend/View/AdminPage.xaml.cs
+++ Frontend/Frontend/View/AdminPage.xaml.cs
@@ -1,15 +1,28 @@
-﻿using System.Windows.Controls;
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Windows.Controls;
 
 namespace Frontend.View
 {
+    
     /// <summary>
     /// Interaction logic for AdminPage.xaml
     /// </summary>
+
     public partial class AdminPage : Page
     {
         public AdminPage()
         {
+       
+
             InitializeComponent();
+
+            ObservableCollection<string> subject = new ObservableCollection<string>();
+            subject.Add("Medieninformatik");
+            subject.Add("Informatik");
+            subjectComboBox.ItemsSource = subject;
+
+
         }
     }
 }
