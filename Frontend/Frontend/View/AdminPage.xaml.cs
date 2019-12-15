e444206a62ca4664509d7603a4bc0f7c047ad477
Leonie Hilgert <leonieluise.hilgert@student.hs-rm.de>
2019-12-15 17:33:18 +0100

    add delete-button and style changes

--- Frontend/Frontend/View/AdminPage.xaml.cs
+++ Frontend/Frontend/View/AdminPage.xaml.cs
@@ -1,35 +1,52 @@
 ﻿using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Windows.Controls;
-
+using System.Reflection;
+using System.Windows;
+using System.Windows.Media;
+using Frontend.Models;
 
 namespace Frontend.View
 {
-    
+
+
     /// <summary>
     /// Interaction logic for AdminPage.xaml
     /// </summary>
 
     public partial class AdminPage : Page
     {
+        ObservableCollection<string> subject = new ObservableCollection<string>();
+
         public AdminPage()
         {
-       
-
             InitializeComponent();
+            /*StudyProgram test2 = new StudyProgram();
+            List<int> test = new List<int>();
+            test = test2.FieldsOfStudies;*/
 
-            ObservableCollection<string> subject = new ObservableCollection<string>();
+
             subject.Add("Medieninformatik");
             subject.Add("Informatik");
             subjectComboBox.ItemsSource = subject;
 
+            ObservableCollection<string> po = new ObservableCollection<string>();
+            po.Add("PO 2019");
+            po.Add("PO 2018");
+            poComboBox.ItemsSource = po;
 
+        }
+
+        private void subjectComboBox_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+        {
+            object selectedSubject =(subjectComboBox.SelectedItem as PropertyInfo).GetValue(null, null);
+            this.Background = new SolidColorBrush(Color.FromRgb(55,100, 5));
         }
 
         private void Button_Click(object sender, System.Windows.RoutedEventArgs e)
         {
-            JsonFileDialog jfd = new JsonFileDialog();
-            jfd.GetJsonFromFile();
+           // JsonFileDialog jfd = new JsonFileDialog();
+            //jfd.GetJsonFromFile();
         }
     }
 }
