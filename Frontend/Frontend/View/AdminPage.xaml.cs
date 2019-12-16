a428d70b4cb4ccbe8a58f160340ec27c11ba2001
Leonie Hilgert <leonieluise.hilgert@student.hs-rm.de>
2019-12-16 10:59:35 +0100

    combobox filled with a list

--- Frontend/Frontend/View/AdminPage.xaml.cs
+++ Frontend/Frontend/View/AdminPage.xaml.cs
@@ -16,41 +16,39 @@ namespace Frontend.View
 
     public partial class AdminPage : Page
     {
-        ObservableCollection<string> subject = new ObservableCollection<string>();
+       
 
         public AdminPage()
         {
             InitializeComponent();
-            /*StudyProgram test2 = new StudyProgram();
-            List<int> test = new List<int>();
-            test = test2.FieldsOfStudies;*/
-
-            University university = new University();
-            HashSet<FieldOfStudy> fieldOfStudies= university.FieldOfStudies;
-
-            fieldofStudyComboBox.ItemsSource = fieldOfStudies;
-
-            subject.Add("Medieninformatik");
-            subject.Add("Informatik");
-            subjectComboBox.ItemsSource = subject;
-
-            ObservableCollection<string> po = new ObservableCollection<string>();
-            po.Add("PO 2019");
-            po.Add("PO 2018");
-            poComboBox.ItemsSource = po;
-
+           
         }
 
         private void subjectComboBox_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
         {
-            object selectedSubject =(subjectComboBox.SelectedItem as PropertyInfo).GetValue(null, null);
-            this.Background = new SolidColorBrush(Color.FromRgb(55,100, 5));
+            if (subjectComboBox.SelectedItem != null)
+            {
+                //object selectedSubject = (subjectComboBox.SelectedItem as PropertyInfo).GetValue(null, null);
+                //this.Background = new SolidColorBrush(Color.FromRgb(55, 100, 5));
+            }
         }
 
         private void fieldoStudyComboBox_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
         {
-            object selectedSubject = (subjectComboBox.SelectedItem as PropertyInfo).GetValue(null, null);
-            this.Background = new SolidColorBrush(Color.FromRgb(55, 100, 5));
+            if (subjectComboBox.SelectedItem != null)
+            {
+                object selectedSubject = (subjectComboBox.SelectedItem as PropertyInfo).GetValue(null, null);
+                this.Background = new SolidColorBrush(Color.FromRgb(55, 100, 5));
+            }
+        }
+
+        private void poComboBox_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+        {
+            if (subjectComboBox.SelectedItem != null)
+            {
+                object selectedSubject = (subjectComboBox.SelectedItem as PropertyInfo).GetValue(null, null);
+                this.Background = new SolidColorBrush(Color.FromRgb(55, 100, 5));
+            }
         }
 
         private void Button_Click(object sender, System.Windows.RoutedEventArgs e)
---
+++ Frontend/Frontend/ViewModel/AdminpageViewModel.cs
@@ -0,0 +1,34 @@
+﻿using Frontend.Helpers;
+using Frontend.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Frontend.ViewModel
+{
+    class AdminpageViewModel: ViewModelBase
+    {
+        private ObservableCollection<string> myItems = new ObservableCollection<string>() { 
+        "one", "two","three" };
+        public AdminpageViewModel()
+        {
+            University university = new University();
+            HashSet<FieldOfStudy> fieldOfStudies = university.FieldOfStudies;
+
+            //fieldofStudyComboBox.ItemsSource = fieldOfStudies;
+
+            
+            //subject.Add("Medieninformatik");
+            //subject.Add("Informatik");
+            //subjectComboBox.ItemsSource = subject;
+
+            ObservableCollection<string> po = new ObservableCollection<string>();
+            po.Add("PO 2019");
+            po.Add("PO 2018");
+           // poComboBox.ItemsSource = po;
+        }
+    }
+}
