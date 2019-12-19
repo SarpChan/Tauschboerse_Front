71b1341f7213f60e29c7f8c11c1baed38f4f32ca
Marc-André Rohde <marc-andre.rohde@student.hs-rm.de>
2019-12-19 18:18:30 +0100

    added first usercontrols

---
+++ Frontend/Frontend/UserControls/Login_HeadText.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Frontend.UserControls
+{
+    /// <summary>
+    /// Interaction logic for Login_HeadText.xaml
+    /// </summary>
+    public partial class Login_HeadText : UserControl
+    {
+        public Login_HeadText()
+        {
+            InitializeComponent();
+        }
+    }
+}
