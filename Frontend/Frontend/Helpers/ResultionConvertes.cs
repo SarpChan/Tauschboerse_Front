dbd4c1d0bdceaec22b32aaa59e2c22f35dcda26b
Lukas <dr.kaese@live.com>
2019-12-25 21:48:34 +0100

    Dynamisches Convertiern von Points To Pixels (je nach Bildschirm DPI)

--- Frontend/Frontend/Helpers/ResultionConvertes.cs
+++ Frontend/Frontend/Helpers/ResultionConvertes.cs
@@ -1,22 +1,34 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace Frontend.Helpers
 {
     class PixelCalculator
     {
+        
+
         public static double PointsToPixels(double points)
         {
-            return points * (96.0 / 72.0);
+            return points * (CalculateDpiX()/ 72);
         }
 
         public static double PixelsToPoints(double pixels)
         {
-            return pixels / (96.0 / 72.0);
+            return pixels * (72 /CalculateDpiX());
         }
+
+        private static double CalculateDpiX()
+        {
+            var dpiXProperty = typeof(SystemParameters).GetProperty("DpiX", BindingFlags.NonPublic | BindingFlags.Static);
+            return (int)dpiXProperty.GetValue(null, null);
+
+        }
+
     }
    
 }
