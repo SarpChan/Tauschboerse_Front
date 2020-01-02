8f7f3fc15a2289b1b1e1b2b54b8d02b1090a2068
Lukas <dr.kaese@live.com>
2020-01-02 16:30:25 +0100

    Farben koennen mir Fixen werten in der App.config generiert werden o. mit hashing des Names / des Typen. Einstellungen sind in der App.config

---
+++ Frontend/Frontend/Helpers/Generators/ColorGenerator.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace Frontend.Helpers.Generators
+{
+    public class ColorGenerator
+    {
+
+        static MD5 Md5Hash = MD5.Create();
+
+        public static void generateColor(ModuleDummy moduleDummy)
+        {
+            string mode = ConfigurationManager.AppSettings.Get("colorgenerator.mode");
+
+            switch(mode){
+                case "hash": moduleDummy.Color = GenerateHashColor(moduleDummy); break;
+                default: moduleDummy.Color = generateFixColorByType(moduleDummy); break;
+            }
+
+        }
+
+        static string GenerateHashColor(ModuleDummy module)
+        {
+
+            string assigment = ConfigurationManager.AppSettings.Get("colorgenerator.hash.assigment");
+            switch (assigment)
+            {
+                case "byGroup": return GenerateHashColorByGroup(module);
+                default: return GenerateHashColorByType(module);
+            }
+        }
+
+        static string GenerateHashColorByGroup(ModuleDummy module)
+        {
+            return HashStringToColorString(module.CourseName);
+        }
+
+        static string GenerateHashColorByType(ModuleDummy module)
+        {
+            return HashStringToColorString(module.Type.ToString());
+        }
+
+        static string HashStringToColorString(string hashingSource)
+        {
+            var hash = Md5Hash.ComputeHash(Encoding.UTF8.GetBytes(hashingSource));
+            Color color = Color.FromRgb(hash[0], hash[1], hash[2]);
+
+            return color.ToString();
+        }
+
+
+        static String generateFixColorByType(ModuleDummy module)
+        {
+            switch (module.Type)
+            {
+                case ModuleDummy.ModuleType.Praktikum: return ConfigurationManager.AppSettings.Get("colorgenerator.fix.practicaltraining");
+                case ModuleDummy.ModuleType.Tutorium: return ConfigurationManager.AppSettings.Get("colorgenerator.fix.tutorial");
+                case ModuleDummy.ModuleType.Übung: return ConfigurationManager.AppSettings.Get("colorgenerator.fix.exercise");
+                default: return ConfigurationManager.AppSettings.Get("colorgenerator.fix.lecture");
+
+            }
+
+        }
+    }
+}
