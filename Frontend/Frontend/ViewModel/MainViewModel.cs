685c89e60b3d4cf7c77c33f3131f6f26aa70f34c
jmuel004 <julius.mueller@student.hs-rm.de>
2020-01-07 20:03:19 +0100

    small bug

--- Frontend/Frontend/ViewModel/MainViewModel.cs
+++ Frontend/Frontend/ViewModel/MainViewModel.cs
@@ -218,7 +218,7 @@ namespace Frontend.ViewModel
             {
                 App.notifier.ShowError("Der Authentifizierungs-Token ist nicht mehr gueltig");
             }
-        }
+        
             APIClient apiClient = APIClient.Instance;
             apiClient.Logout();
             App.notifier.ShowSuccess("Ausloggen erfolgreich");
