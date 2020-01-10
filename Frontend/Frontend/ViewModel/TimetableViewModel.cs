dfb3ed1f6ce48f4e663034f7eb385f36e4564871
sanja <sanja.blechschmidt@web.de>
2020-01-10 18:41:52 +0100

    SwapOffer Model

--- Frontend/Frontend/ViewModel/TimetableViewModel.cs
+++ Frontend/Frontend/ViewModel/TimetableViewModel.cs
@@ -1,5 +1,7 @@
 ﻿using Frontend.Helpers;
 using Frontend.Models;
+using System;
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
 
 namespace Frontend.ViewModel
@@ -25,6 +27,38 @@ namespace Frontend.ViewModel
                 _DayList.Add(day);
             }
         }
+
+        // Hilfsmethode für AddEinkaufCommand
+        private void AddModule()
+        {
+
+
+            /*
+             * Annahme: Model ist nicht "observer-fähig" (z.B: Datenbank)
+             * daher synchrone Pflege von Model UND ViewModel hier - besser: 
+             * Model mit INotifyPropertyChanged und Änderungen observieren,
+             * Anpassung des ViewModels bei Änderungsmitteilung vom Model
+             */
+            Console.WriteLine("I was here");
+            ModuleDummy add = new ModuleDummy()
+            {
+                ID = "420",
+                StartTime = "08:15",
+                EndTime = "12:30",
+                Day = "4",
+                PersonName = "Nicky",
+                RoomNumber = "D40",
+                CourseName = "TEST",
+                GroupChar = 'Z',
+                Color = "#FFF4A233"
+            };
+            moduleListModel.AddModule(add);
+            ModuleList.Add(add);
+
+        }
+
+        #endregion
+
 
         #region Properties
         private ObservableCollection<TimetableModule> _ModuleList = new ObservableCollection<TimetableModule>();
