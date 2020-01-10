21720ca698910e419d8fd2b2b656d0c0f2900aac
sanja <sanja.blechschmidt@web.de>
2020-01-10 11:59:20 +0100

    DialogService und SwapButton als UC angelegt

---
+++ Frontend/Frontend/UserControls/TT_SwapButton.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Frontend.UserControls
+{
+    /// <summary>
+    /// Interaction logic for TT_SwapButton.xaml
+    /// </summary>
+    public partial class TT_SwapButton : UserControl
+    {
+        public TT_SwapButton()
+        {
+            InitializeComponent();
+        }
+        public ICommand TT_SwapButtonCommand
+        {
+            get { return (ICommand)GetValue(TT_SwapButtonCommandProperty); }
+            set { SetValue(TT_SwapButtonCommandProperty, value); }
+        }
+        public static readonly DependencyProperty TT_SwapButtonCommandProperty =
+            DependencyProperty.Register("TT_SwapButtonCommand", typeof(ICommand), typeof(TT_SwapButton), new UIPropertyMetadata(null));
+    }
+}
---
+++ Frontend/Frontend/ViewModel/SO_DialogViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Input;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Frontend.ViewModel
+{
+    class SO_DialogViewModel 
+    {
+        private ICommand openDialogCommand = null;
+        public ICommand OpenDialogCommand { get { return this.openDialogCommand; }
+            set { this.openDialogCommand = value; }
+        }
+     
+}
