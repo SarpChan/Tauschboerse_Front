682e71512cb2250ee8f9e2918294a917463d699f
tfalk001 <tfalk001@PC42-103>
2020-01-22 14:47:13 +0100

    SwapOffer repariert und Modelle wiederbelebt

--- Frontend/Frontend/Helpers/SwapOfferFromBackend.cs
+++ Frontend/Frontend/Helpers/SwapOfferFromBackend.cs
@@ -1,12 +1,41 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Frontend.Models;
 
 namespace Frontend.Helpers
 {
     class SwapOfferFromBackend
     {
+        SwapOfferListModel swapOfferListModel;
+        public SwapOfferFromBackend()
+        {
+            swapOfferListModel = SwapOfferListModel.Instance;
+        }
+
+
+        private async Task RequestNewsFromServerAsync(long id)
+        {
+
+            SwapOfferFilter swapOfferFilter = new SwapOfferFilter
+            {
+                attribute = "forOwnderId",
+                comperatorValue = id,
+                comperatorType = "EQUALS"
+            };
+            List<SwapOfferFrontendModel> swapOffer = new List<SwapOfferFrontendModel>();
+            APIClient apiClient = APIClient.Instance;
+            var response = await apiClient.NewPOSTRequest("/rest/lists/swapOffer", JsonConvert.SerializeObject(swapOffer, Formatting.Indented));
+            if ((int)response.StatusCode >= 400)
+            {
+                return;
+            }
+            swapOffer = JsonConvert.DeserializeObject<List<SwapOfferFrontendModel>>(response.Content.ToString());
+            swapOfferListModel.SetList(swapOffer);
+        }
+
     }
 }
---
+++ Frontend/Frontend/Models/SwapOfferFrontendModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Frontend.Models
+{
+    class SwapOfferFrontendModel
+    {
+
+        public long Id { get; set; }
+        public char FromGroupChar { get; set; }
+        public char ToGroupChar { get; set; }
+        public string CourseName { get; set; }
+        public string CourseType { get; set; }
+        public TimeSpan ToStartTime { get; set; }
+        public TimeSpan ToEndTime { get; set; }
+        public TimeSpan FromStartTime { get; set; }
+        public TimeSpan FromEndTime { get; set; }
+        public Models.DayOfWeek FromDay { get; set; }
+        public Models.DayOfWeek ToDay { get; set; }
+
+
+    }
+}
