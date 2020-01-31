9fdfbc4af0a8adf7ac5d46878294c6866505b135
sanja <sanja.blechschmidt@web.de>
2020-01-31 10:02:31 +0100

    requestanfrage für swapoffer create

--- Frontend/Frontend/View/Windows/SO_Dialog.xaml.cs
+++ Frontend/Frontend/View/Windows/SO_Dialog.xaml.cs
@@ -69,7 +69,7 @@ namespace Frontend.View
            
             SwapOffer so = new SwapOffer(FromGroupID, ToGroupId);
             APIClient apiClient = APIClient.Instance;
-            var response = await apiClient.NewPOSTRequest("/rest/swapOffer/create", so);
+            var response = await apiClient.NewPOSTRequest("/rest/swapoffer/insert", so);
             Console.WriteLine(response.Content);
             if ((int)response.StatusCode >= 400) return;
             Console.WriteLine(response.Content);
