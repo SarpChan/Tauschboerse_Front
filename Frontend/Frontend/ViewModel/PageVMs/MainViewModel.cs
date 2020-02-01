6a97b81472de595ade71a664a96e5e6c88a1dc52
nahle001 <nicklas.ahlers@student.hs-rm.de>
2020-02-01 17:45:25 +0100

    fixed merge issues

--- Frontend/Frontend/ViewModel/PageVMs/MainViewModel.cs
+++ Frontend/Frontend/ViewModel/PageVMs/MainViewModel.cs
@@ -32,7 +32,7 @@ namespace Frontend.ViewModel
         private static MainViewModel _instance;
         public static MainViewModel Instance { get { return _instance; } }
 
-        //SwapOfferMessageBroker so_mb;
+        SwapOfferMessageBroker so_mb;
 
         public MainViewModel()
         {
@@ -42,7 +42,7 @@ namespace Frontend.ViewModel
             timetableModuleList = ModuleListModel.Instance;
             thisID = (int)(new Random().NextDouble() * 9999) + 1;
             //Console.WriteLine("\"NEW MAIN_VIEWMODEL\" InstanceID: "  + thisID);
-            //so_mb = new SwapOfferMessageBroker();
+            so_mb = new SwapOfferMessageBroker();
             _instance = this;
         }
 
